11064d5b112c1ffc727a5b8dcbaddf9550eecb3a
Ирина Пересыпкина <irina_p@udev.dev>
2023-05-24 19:42:46 +0300

    Initial Commit

---
+++ JustCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustCards
+//
+//  Created by Ирина Пересыпкина on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
